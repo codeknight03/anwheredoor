5b991be5020db051533ae4e8be9eab61376a093c
Codeknight03 <kanishkramwani6@gmail.com>
2025-01-03 04:45:40 +0530

    docs(examples): Updated the basic rp example.

--- example/basic-rp/three-servers.sh
+++ example/basic-rp/three-servers.sh
@@ -3,9 +3,9 @@
 # Start demo services
 start_containers() {
   echo "Starting demo HTTP servers..."
-  docker run --rm -d -p 9001:80 --name server1 kennethreitz/httpbin
-  docker run --rm -d -p 9002:80 --name server2 kennethreitz/httpbin
-  docker run --rm -d -p 9003:80 --name server3 kennethreitz/httpbin
+  docker run --rm -d -p 9001:80 --name server1 -e SERVER_NAME="Server 1" hashicorp/http-echo -listen=:80 -text="Hello from Server 1"
+  docker run --rm -d -p 9002:80 --name server2 -e SERVER_NAME="Server 2" hashicorp/http-echo -listen=:80 -text="Hello from Server 2"
+  docker run --rm -d -p 9003:80 --name server3 -e SERVER_NAME="Server 3" hashicorp/http-echo -listen=:80 -text="Hello from Server 3"
   echo "Servers are running at:"
   echo " - http://localhost:9001"
   echo " - http://localhost:9002"
